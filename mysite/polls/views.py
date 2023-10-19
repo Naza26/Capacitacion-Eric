4afc9db5e70f52f5c3c410c5d58f9cfe8c307cc2
Nazarena Rueda <naza120@gmail.com>
2023-10-19 11:48:01 -0300

    wip

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -30,12 +30,6 @@ class ResultsView(generic.DetailView):
     template_name = "polls/results.html"
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
-
-
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
