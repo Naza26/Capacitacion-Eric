847506865a67afd5efd06e65415b806e9a249bd4
Nazarena Rueda <naza120@gmail.com>
2023-10-19 15:51:59 -0300

    wip

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -27,11 +27,6 @@ class ResultsView(generic.View):
         return render(request, "polls/results.html", {"question": question})
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
-
-
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
