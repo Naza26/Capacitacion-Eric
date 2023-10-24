7a564c529a67bda15b79fbc147c0f35ba98a6e0a
Nazarena Rueda <50343570+Naza26@users.noreply.github.com>
2023-10-24 10:20:53 -0300

    wip

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -51,7 +51,16 @@ class ResultsView(generic.View):
 class XXX:
     def __init__(self, error, question):
         self.error = error
-        self.question = question
+        self._question = question
+
+    def question(self):
+        return self._question
+
+    def question_does_not_exist(self):
+        return self.error == "Question does not exist"
+
+    def choice_does_not_exist(self):
+        return self.error == "Choice does not exist."
 
 
 class VoteView(generic.View):
@@ -59,22 +68,21 @@ class VoteView(generic.View):
     def post(self, request, question_id):
         choice_id = request.POST["choice"]
 
-        an_error_occurred, question = self._add_vote_to_choice(question_id, choice_id)
-        result = XXX(an_error_occurred, question)
+        result = self._add_vote_to_choice(question_id, choice_id)
 
-        if result.error == "Question does not exist":
+        if result.question_does_not_exist():
             raise Http404("Question does not exist")
-        if an_error_occurred == "Choice does not exist.":
+        if result.choice_does_not_exist():
             return render(
                 request,
                 "polls/detail.html",
                 {
-                    "question": question,
+                    "question": result.question(),
                     "error_message": "You didn't select a choice.",
                 },
             )
         else:
-            return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+            return HttpResponseRedirect(reverse("polls:results", args=(result.question().id,)))
 
     def _add_vote_to_choice(self, question_id, choice_id):
         error = None
@@ -82,14 +90,14 @@ class VoteView(generic.View):
             question = self._published_question(question_id)
         except Question.DoesNotExist:
             error = "Question does not exist"
-            return error, None
+            return XXX(error, None)
         try:
             selected_choice = self._selected_choice(choice_id, question)
             selected_choice.votes += 1
             selected_choice.save()
         except (KeyError, Choice.DoesNotExist):
             error = "Choice does not exist."
-        return error, question
+        return XXX(error, question)
 
     def _selected_choice(self, choice_id, question):
         selected_choice = question.choice_set.get(pk=choice_id)
